252c29dfe115fc0ff504ac15c52dd427c4b79a8c
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2013-10-17 17:31:33 +0400

    Scripts updated

--- client/run.sh
+++ client/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 JRE_HOME=/local/opt/jdk1.6.0/jre
-$JRE_HOME/bin/java -cp ./target/client.jar org.soapdemo.client.Client -h localhost -p 8080
+$JRE_HOME/bin/java -cp ./target/client.jar org.soapdemo.client.Client \
+	-h localhost -p 8080
--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 JRE_HOME=/local/opt/jdk1.6.0/jre
-$JRE_HOME/bin/java -cp ./target/server.jar:./lib/connector-api-1.5.jar:./lib/jaybird-2.2.3.jar org.soapdemo.server.Server -l localhost -p 8080 -url jdbc:firebirdsql:localhost/3050:/local/work/soap/db/INFO.fdb?encoding=WIN1251 -user sysdba -pass masterkey
+$JRE_HOME/bin/java -cp ./target/server.jar:./lib/connector-api-1.5.jar:./lib/jaybird-2.2.3.jar org.soapdemo.server.Server \
+	-l localhost -p 8080 \
+	-url jdbc:firebirdsql:localhost/3050:/local/work/soap/db/INFO.fdb?encoding=WIN1251 \
+	-user sysdba -pass masterkey
